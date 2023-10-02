2adc37a1218873178c1d419575fe6af6edcf7346
Marco-DG <dg.account@protonmail.com>
2023-10-02 17:38:25 +0200

    .

--- src/sy/utils/CLI.java
+++ src/sy/utils/CLI.java
@@ -39,7 +39,11 @@ public class CLI
 	{
 	    try
 	    {
-	        new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+			if (System.getProperty("os.name").contains("Windows"))
+				new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+			else
+				System.out.print("\033[H\033[2J");
+				System.out.flush();
 	    }
 	    catch (final Exception e)
 	    {
